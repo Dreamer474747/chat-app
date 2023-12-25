3485f46ec30065a91aba9c8c9f5d564a0aaac3cc
Mobin taataghi <123218770+Dreamer474747@users.noreply.github.com>
2023-12-25 16:10:05 +0330

    Delete src directory

--- src/helpers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as colorPicker } from "./colorPicker";
-export { default as timeConverter } from "./timeConverter";
-export { default as lastMessagesUpdater } from "./lastMessagesUpdater";
